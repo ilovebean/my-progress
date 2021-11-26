9367e0b8a0d911b6a591daed7a616c942a6c73a4
Camopass <71544697+Camopass@users.noreply.github.com>
2021-11-25 22:33:25 -0600

    PEP8
    Fulfill PEP-8 conventions for better readability and organization.

--- LevelSelect.py
+++ LevelSelect.py
@@ -1,18 +1,23 @@
 import pygame
 import sys
+import time
+
 from pygame.locals import *
 from Engine.UserInterface import Button
-import time
+
 
 def main():
     pygame.init()
 
     lvl_screen = pygame.display.set_mode((400, 400))
     time.sleep(2)
+    
     while True:
         for event in pygame.event.get():
-            if event.type==QUIT:
+            if event.type == QUIT:
                 pygame.quit()
-                sys.exit() 
-        
-main()
+                sys.exit()
+                
+if __name__ == '__main__':
+    main()
+    
