47994bb18ca8d67309cddea726a9586ed71ba885
Camopass <71544697+Camopass@users.noreply.github.com>
2021-11-25 22:44:26 -0600

    Refactors/Optimization
    I have done what I can to help make this more readable and optimized. Please for the love of god at least name your variables something other than image 1 2 3 4 5 6. You also might want to look over PEP-8 a bit, it really helps with readability.

--- title_screen.py
+++ title_screen.py
@@ -1,101 +1,104 @@
-import pygame
 import sys
-from pygame.locals import *
-from Engine.UserInterface import Button
 import time
+
+import pygame
+from pygame.locals import *
+
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-    green_1 = (55, 138, 80, 0.8)
+    green_1 = (55, 138, 80)
 
     screen.fill(green_1)
 
     x, y = screen.get_size()
     counterx = x
     countery = y
-        
-    print(x,y)#debug
 
-    x1, y1 = pygame.mouse.get_pos()
-    counterx1 = x1
-    countery1 = y1
+    print(x, y)  # debug
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    counterx1 = mouse_x
+    countery1 = mouse_y
 
     print(counterx1, countery1)
 
-    image_1 = pygame.image.load(r"assets/UI/menu_background.png")#background
-    image_2 = pygame.image.load(r"assets/UI/Start_button.png")#start Button
-    image_2_2 = pygame.image.load(r"assets/UI/Start_button.png")#start Button
-    image_3 = pygame.image.load(r"assets/UI/Start_button_hover.png")#start Button hover
-    image_4 = pygame.image.load(r"assets/UI/exit_button.png")#exit button
-    image_5 = pygame.image.load(r"assets/UI/Title.png")#Title
-    image_6 = pygame.image.load(r"assets/icons/16.png")
+    background = pygame.image.load(r"assets/UI/menu_background.png")  # background
+    start_button = pygame.image.load(r"assets/UI/Start_button.png")  # start Button
+    start_button_resize = start_button.copy()  # start Button Resized
+    hovered_start_button = pygame.image.load(r"assets/UI/Start_button_hover.png")  # start Button hover
+    exit_button = pygame.image.load(r"assets/UI/exit_button.png")  # exit button
+    title = pygame.image.load(r"assets/UI/Title.png")  # Title
+    icon = pygame.image.load(r"assets/icons/16.png")  # Icon
 
     size = (x, y)
-    image = pygame.transform.scale(image_1, size)
-    
-    while True:
-        for event in pygame.event.get():
-            if event.type==QUIT:
-                pygame.quit()
-                sys.exit() 
+    image = pygame.transform.scale(background, size)
 
+    pygame.display.set_icon(icon)
+
+    while True:
         x, y = screen.get_size()
         if x != counterx:
             counterx = x
-            size = (x,y)
-            image = pygame.transform.scale(image_1, size)
-        
+            size = (x, y)
+            image = pygame.transform.scale(background, size)
+
         if y != countery:
             countery = y
-            size = (x,y)
-            image = pygame.transform.scale(image_1, size)
+            size = (x, y)
+            image = pygame.transform.scale(background, size)
 
-        x1, y1 = pygame.mouse.get_pos()#Hover effects
+        mouse_x, mouse_y = pygame.mouse.get_pos()  # Hover effects
 
-        start_x_1 = (counterx/4)
-        start_x_2 = (start_x_1 + (counterx/4))
-        start_y_1 = ((countery/5)*3)
-        start_y_2 = (start_y_1 + (countery/8))
+        start_x_1 = (counterx / 4)
+        start_x_2 = (start_x_1 + (counterx / 4))
+        start_y_1 = ((countery / 5) * 3)
+        start_y_2 = (start_y_1 + (countery / 8))
 
-        exit_x_1 = ((x - counterx/16))
+        exit_x_1 = (x - counterx / 16)
         exit_x_2 = (x)
         exit_y_1 = (0)
-        exit_y_2 = (0 + (counterx/16))
+        exit_y_2 = (0 + (counterx / 16))
 
-        if start_x_1<x1<start_x_2:
-            if start_y_1<y1<start_y_2:
-                image_2 = image_3
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x2, y2 = pygame.mouse.get_pos()
+                if exit_x_1 < x2 < exit_x_2:
+                    if exit_y_1 < y2 < exit_y_2:
+                        pygame.quit()
+                        sys.exit()
+                if start_x_1 < mouse_x < start_x_2:
+                    if start_y_1 < mouse_y < start_y_2:
+                        pygame.quit()
+
+                        time.sleep(1)
+
+        if start_x_1 < mouse_x < start_x_2:
+            if start_y_1 < mouse_y < start_y_2:
+                start_button = hovered_start_button
             else:
-                image_2 = image_2_2
+                start_button = start_button_resize
         else:
-            image_2 = image_2_2
+            start_button = start_button_resize
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            x2, y2 = pygame.mouse.get_pos()
-            if exit_x_1< x2< exit_x_2:
-                if exit_y_1<y2<exit_y_2:
-                    pygame.quit()
-                    sys.exit()
-            if start_x_1<x1<start_x_2:
-                if start_y_1<y1<start_y_2:
-                    pygame.quit()
-                    import LevelSelect
+        scaled_start_button = pygame.transform.scale(start_button,
+                                        ((counterx / 4), (countery / 8)))  # Constantly updates the size of the button
+        scaled_exit_button = pygame.transform.scale(exit_button, ((counterx / 16), (counterx / 16)))
+        scaled_title = pygame.transform.scale(title, (((counterx * 3) / 4), ((countery / 7) * 3)))
 
-                    time.sleep(1)
-
-        image1 = pygame.transform.scale(image_2, ((counterx/4), (countery/8)))  #Constantly updates the size of the button
-        image2 = pygame.transform.scale(image_4, ((counterx/16), (counterx/16)))
-        image3 = pygame.transform.scale(image_5, (((counterx * 3)/4), ((countery/7)*3)))
-
-        pygame.display.set_icon(image_6)
-        screen.blit(image, [0, 0])#Background
-        screen.blit(image1, ((counterx/4), ((countery/5)*3)))#start button pos
-        screen.blit(image2, (((x - counterx/16)), (0)))
-        screen.blit(image3, (0,0))
+        
+        screen.blit(image, (0, 0))  # Background
+        screen.blit(scaled_start_button, ((counterx / 4), ((countery / 5) * 3)))  # start button pos
+        screen.blit(scaled_exit_button, ((x - counterx / 16), 0))
+        screen.blit(scaled_title, (0, 0))
         pygame.display.update()
-            
+
+
 if __name__ == '__main__':
     main()
